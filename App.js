c850ed2268db187393658a766cba73056a81d47b
Sultan Javier <sultan.javier@gmail.com>
2024-01-12 21:04:59 -0800

    fir

--- App.js
+++ App.js
@@ -6,16 +6,44 @@ import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   return (
     <View style={styles.container}>
-      <LinearGradient colors={['#4c669f', '#3b5998', '#000000']} style={styles.linearGradient}>
+      <LinearGradient colors={['#4c669f', '#3b5998', '#fff']} style={styles.linearGradient}>
         
         <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>
+          Attendance Management System
+        </Text>
         <StatusBar style="auto" />
+
+        <View style={styles.bottom}>
+          <Text style={styles.poweredBy}>powered by andes</Text>
+          </View>
       </LinearGradient>
     </View>
+
+      
+      
   );
 }
 
 const styles = StyleSheet.create({
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 16,
+    alignItems: 'center',
+  },
+  poweredBy: {
+    fontSize: 12,
+    color: 'black', // Use a low-opacity gray color for the watermark
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 14,
+    fontFamily: 'FontMontserrat', // Make sure to include the font family
+    textAlign: 'center',
+  },
   linearGradient: {
     flex: 1,
     paddingLeft: 15,
